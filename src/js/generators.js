515d052a39e621de881bc9387a17059ac93577dd
neondoll <bonni_1998@mail.ru>
2024-05-07 18:46:29 +0300

    Формирование команд

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,5 @@
+import Team from './Team';
+
 /**
  * Формирует экземпляр персонажа из массива allowedTypes со
  * случайным уровнем от 1 до maxLevel
@@ -10,15 +12,31 @@
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
+  while (true) {
+    const CharacterClass = allowedTypes[Math.random() * allowedTypes.length];
+    const characterLevel = (Math.random() * maxLevel) + 1;
+
+    yield new CharacterClass(characterLevel);
+  }
 }
 
 /**
  * Формирует массив персонажей на основе characterGenerator
  * @param allowedTypes массив классов
  * @param maxLevel максимальный возможный уровень персонажа
- * @param characterCount количество персонажей, которое нужно сформировать
- * @returns экземпляр Team, хранящий экземпляры персонажей. Количество персонажей в команде - characterCount
- * */
+ * @param characterCount количество персонажей, которое нужно
+ * сформировать
+ * @returns экземпляр Team, хранящий экземпляры персонажей.
+ * Количество персонажей в команде - characterCount
+ */
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const characters = [];
+  const playerGenerator = characterGenerator(allowedTypes, maxLevel);
+
+  for (let index = 0; index < characterCount; index += 1) {
+    characters.push(playerGenerator.next().value);
+  }
+
+  return new Team(characters);
 }
